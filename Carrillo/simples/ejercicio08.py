9f690d1e970adca3eb4948c9a02fc9d4ba6a26d8
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:24:38 -0500

    se agregó el ejercicio08

---
+++ Carrillo/simples/ejercicio08.py
@@ -0,0 +1,9 @@
+import os
+#imput
+pi=float(os.sys.argv[1])
+radio=float(os.sys.argv[2])
+#processing
+area_circunferencia=(pi*radio)
+#output
+if(area_circunferencia<=24):
+    print("esta pequeño")
