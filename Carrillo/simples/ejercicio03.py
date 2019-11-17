a537f9ec0601ae23353d853bb0e4125e00a8dc4e
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:21:34 -0500

    se agregó el ejercicio03

---
+++ Carrillo/simples/ejercicio03.py
@@ -0,0 +1,11 @@
+import os
+#imput
+nota1=int(os.sys.argv[1])
+nota2=int(os.sys.argv[2])
+nota3=int(os.sys.argv[3])
+#processing
+promedio=(nota1+nota2+nota3)/3
+#output
+if (promedio> 15):
+    print("estudia más")
+#fin_if
