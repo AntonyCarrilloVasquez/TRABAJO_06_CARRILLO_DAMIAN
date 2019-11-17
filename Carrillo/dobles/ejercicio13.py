82dd146b419804caad798cd8a849be5f732984e6
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:26:58 -0500

    se agregó el ejercicio13

---
+++ Carrillo/dobles/ejercicio13.py
@@ -0,0 +1,13 @@
+import os
+#imput
+nota1=int(os.sys.argv[1])
+nota2=int(os.sys.argv[2])
+nota3=int(os.sys.argv[3])
+#processing
+promedio=(nota1+nota2+nota3)/3
+#output
+if (promedio> 15):
+    print("sigue así")
+else:
+    print("estudi más")
+#fin_if
