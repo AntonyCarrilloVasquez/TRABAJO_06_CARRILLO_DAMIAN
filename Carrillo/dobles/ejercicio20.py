edcb7a4d68227fba4880fe412c3512b0a09a55f1
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:29:06 -0500

    se agregó el ejercicio20

---
+++ Carrillo/dobles/ejercicio20.py
@@ -0,0 +1,20 @@
+import os
+#imput
+masa=int(os.sys.argv[1])
+volumen=int(os.sys.argv[2])
+#processing
+densidad=(masa/volumen)
+#output
+print("#####################")
+print(" densidad de un cuerpo ")
+print("#####################")
+print("#")
+print("masa:              ",masa," ")
+print("volumen:           ",volumen," ")
+print("densidad           ",densidad," ")
+print("#####################")
+#condicionales simples
+if (densidad<=39):
+    print("la densidad es aceptable")
+else:
+    print("esta en estado desfavorable")
