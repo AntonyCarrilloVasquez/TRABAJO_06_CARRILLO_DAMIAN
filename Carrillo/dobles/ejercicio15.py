9f3a97fc10df45d7992a567b4afa64f777491db7
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:27:38 -0500

    se agregó el ejercicio15

---
+++ Carrillo/dobles/ejercicio15.py
@@ -0,0 +1,20 @@
+import os
+#imput
+masa=int(os.sys.argv[1])
+aceleracion=int(os.sys.argv[2])
+#processing
+fuerza=(masa*aceleracion)
+#output
+print("#####################")
+print(" fuerza de una persona ")
+print("#####################")
+print("#")
+print("masa:                ",masa," ")
+print("aceleracion:         ",aceleracion," ")
+print("fuerza               ",fuerza," ")
+print("#####################")
+#condicionales simples
+if(fuerza<12):
+    print("eres muy debil")
+else:
+    print("sigue asi")
