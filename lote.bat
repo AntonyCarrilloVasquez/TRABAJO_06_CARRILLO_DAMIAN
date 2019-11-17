547db69bb9ae56d5cbfb33a3980eeaf085e96918
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-17 16:17:59 -0500

    se agregó el lote.bat

---
+++ lote.bat
@@ -0,0 +1,271 @@
+python Carrillo/simples/ejercicio01.py "Juan" 53 14 15 21.5 12.6 14.7 8.8
+python Carrillo/simples/ejercicio01.py "Juan" 25 14 15 61.2 45.6 74.7 8.8
+python Carrillo/simples/ejercicio01.py "Juan" 23 29 12 12.8 1.6 64.7 8.8
+
+python Carrillo/simples/ejercicio02.py 41 31 8
+python Carrillo/simples/ejercicio02.py 51 62 21
+python Carrillo/simples/ejercicio02.py 15 15 6
+
+
+python Carrillo/simples/ejercicio03.py 15 17 18
+python Carrillo/simples/ejercicio03.py 19 12 19
+python Carrillo/simples/ejercicio03.py 13 15 17
+
+python Carrillo/simples/ejercicio04.py 32 15
+python Carrillo/simples/ejercicio04.py 21 15
+python Carrillo/simples/ejercicio04.py 13 15
+
+python Carrillo/simples/ejercicio05.py 24 41
+python Carrillo/simples/ejercicio05.py 62 21
+python Carrillo/simples/ejercicio05.py 13 15
+
+python Carrillo/simples/ejercicio06.py "Pedro" "mango" 41 4
+python Carrillo/simples/ejercicio06.py "Ana" "fresa" 51 4
+python Carrillo/simples/ejercicio06.py "Rosa" "pera" 62 4
+
+python Carrillo/simples/ejercicio07.py 21 52 6
+python Carrillo/simples/ejercicio07.py 61 60 8
+python Carrillo/simples/ejercicio07.py 54 2 21
+
+python Carrillo/simples/ejercicio08.py 3.14 6
+python Carrillo/simples/ejercicio08.py 3.14 7
+python Carrillo/simples/ejercicio08.py 3.14 10
+
+python Carrillo/simples/ejercicio09.py 51 41
+python Carrillo/simples/ejercicio09.py 78 9
+python Carrillo/simples/ejercicio09.py 31 7
+
+python Carrillo/simples/ejercicio10.py 123 7
+python Carrillo/simples/ejercicio10.py 62 8
+python Carrillo/simples/ejercicio10.py 45 12
+
+
+
+
+
+
+python Carrillo/dobles/ejercici11.py "Pedro" 12 42 15 61.5 22.6 12.7 8.8
+python Carrillo/dobles/ejercicio11.py "walter" 25 154 15 71.2 45.6 74.7 8.8
+python Carrillo/dobles/ejercicio11.py "Antony" 41 23 11 2.8 1.6 14.7 8.8
+
+python Carrillo/dobles/ejercicio12.py 21 21 8
+python Carrillo/dobles/ejercicio12.py 54 52 21
+python Carrillo/dobles/ejercicio12.py 15 15 6
+
+
+python Carrillo/dobles/ejercicio13.py 12 11 16
+python Carrillo/dobles/ejercicio13.py 15 17 10
+python Carrillo/dobles/ejercicio13.py 17 15 17
+
+python Carrillo/dobles/ejercicio14.py 15 15
+python Carrillo/dobles/ejercicio14.py 23 17
+python Carrillo/dobles/ejercicio14.py 63 11
+
+python Carrillo/dobles/ejercicio15.py 24 41
+python Carrillo/dobles/ejercicio15.py 62 21
+python Carrillo/dobles/ejercicio15.py 13 15
+
+python Carrillo/dobles/ejercicio16.py "Ebelin" "mango" 12 12
+python Carrillo/dobles/ejercicio16.py "Arturo" "cereza" 71 9
+python Carrillo/dobles/ejercicio16.py "Randol" "mandarina" 67 7
+
+python Carrillo/dobles/ejercicio17.py 17 80 16
+python Carrillo/dobles/ejercicio17.py 51 81 8
+python Carrillo/dobles/ejercicio17.py 58 46 21
+
+python Carrillo/dobles/ejercicio18.py 3.14 10
+python Carrillo/dobles/ejercicio18.py 3.14 7
+python Carrillo/dobles/ejercicio18.py 3.14 6
+
+python Carrillo/dobles/ejercicio19.py 41 41
+python Carrillo/dobles/ejercicio19.py 62 12
+python Carrillo/dobles/ejercicio19.py 61 6
+
+python Carrillo/dobles/ejercicio20.py 13 9
+python Carrillo/dobles/ejercicio20.py 82 7
+python Carrillo/dobles/ejercicio20.py 09 62
+
+
+
+
+
+
+
+
+python Carrillo/multiples/ejercicio21.py "Genaro" 12 42 15 91.5 22.6 22.7 8.8
+python Carrillo/multiples/ejercicio21.py "Rogelio" 43 14 53 71.2 45.7 74.7 8.8
+python Carrillo/multiples/ejercicio21.py "Diego" 21 23 71 12.8 1.6 14.7 8.8
+
+python Carrillo/multiples/ejercicio22.py 34 45 8
+python Carrillo/multiples/ejercicio22.py 62 20 61
+python Carrillo/multiples/ejercicio22.py 65 31 6
+
+
+python Carrillo/multiples/ejercicio23.py 18 17 16
+python Carrillo/multiples/ejercicio23.py 15 20 10
+python Carrillo/multiples/ejercicio23.py 17 10 17
+
+python Carrillo/multiples/ejercicio24.py 16 21
+python Carrillo/multiples/ejercicio24.py 23 18
+python Carrillo/multiples/ejercicio24.py 21 16
+
+python Carrillo/multiples/ejercicio25.py 31 41
+python Carrillo/multiples/ejercicio25.py 21 34
+python Carrillo/multiples/ejercicio25.py 54 19
+
+python Carrillo/multiples/ejercicio26.py "Esteban" "mango" 51 2
+python Carrillo/multiples/ejercicio26.py "Alok" "coca" 31 9
+python Carrillo/multiples/ejercicio26.py "Ronal" "sandia" 77 7
+
+python Carrillo/multiples/ejercicio27.py 17 80 56
+python Carrillo/multiples/ejercicio27.py 27 32 1
+python Carrillo/multiples/ejercicio27.py 58 46 31
+
+python Carrillo/multiples/ejercicio28.py 3.14 21
+python Carrillo/multiples/ejercicio28.py 3.14 2
+python Carrillo/multiples/ejercicio28.py 3.14 14
+
+python Carrillo/multiples/ejercicio29.py 34 21
+python Carrillo/multiples/ejercicio29.py 12 42
+python Carrillo/multiples/ejercicio29.py 62 5
+
+python Carrillo/multiples/ejercicio30.py 53 3
+python Carrillo/multiples/ejercicio30.py 22 7
+python Carrillo/multiples/ejercicio30.py 79 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pitón Damian / dobles / ejercicio01.py 18  120  40
+pitón Damián / dobles / ejercicio01.py 17  45  35
+pitón Damián / dobles / ejercicio01.py 80  14  65
+
+pitón Damian / dobles / ejercicio02.py 120  100  130
+pitón Damian / dobles / ejercicio02.py 130  100  50
+pitón Damian / dobles / ejercicio02.py 200  100  80
+
+pitón Damián / dobles / ejercicio03.py 45  25  30
+pitón Damián / dobles / ejercicio03.py 25  25  25
+pitón Damián / dobles / ejercicio03.py 40  10  19
+
+pitón Damián / dobles / ejercicio04.py 15  15  19
+pitón Damián / dobles / ejercicio04.py 15  18  17
+pitón Damián / dobles / ejercicio04.py 18  19  23
+
+pitón Damián / dobles / ejercicio05.py 15  12  30
+pitón Damián / dobles / ejercicio05.py 17  15  42
+pitón Damián / dobles / ejercicio05.py 20  15  45
+
+python Damian / dobles / ejercicio06.py " juan "  12  17  58
+python Damian / dobles / ejercicio06.py " Andre "  10  75  38
+python Damian / dobles / ejercicio06.py " Alejandra "  100  75  45
+
+python Damian / dobles / ejercicio07.py " juan "  45  35  48
+python Damian / dobles / ejercicio07.py " juan "  75  45  65
+python Damian / dobles / ejercicio07.py " juan "  80  45  64
+
+pitón Damián / dobles / ejercicio08.py " Juan "  18  18  24
+pitón Damián / dobles / ejercicio08.py " Juan "  15  24  36
+pitón Damián / dobles / ejercicio08.py " Juan "  15  15  15
+
+
+
+
+
+python Damián / dobles / ejercicio10.py 12  7  2
+python Damian / dobles / ejercicio10.py 11  7  1
+python Damian / dobles / ejercicio10.py 7  12  5
+
+python Damian / simples / ejercicio11.py 25  17  31
+Python Damian / simples / ejercicio11.py 14  15  35
+python Damian / simples / ejercicio11.py 14  25  35
+
+python Damian / simples / ejercicio12.py 250  240  150
+python Damian / simples / ejercicio12.py 250  140  170
+python Damian / simples / ejercicio12.py 280  120  253
+
+python Damian / simples / ejercicio13.py 15  10  90
+python Damian / simples / ejercicio13.py 15  15  120
+python Damian / simples / ejercicio13.py 30  15  120
+
+python Damian / simples / ejercicio14.py 50  25  35
+python Damian / simples / ejercicio14.py 54  26  35
+python Damián / simples / ejercicio14.py 54  30  45
+
+python Damian / simples / ejercicio15.py 120  75  35
+python Damian / simples / ejercicio15.py 100  75  85
+python Damián / simples / ejercicio15.py 120  45  65
+
+Python Damian / simples / ejercicio16.py 10  5  8
+Python Damian / simples / ejercicio16.py 11  4  3
+python Damian / simples / ejercicio16.py 13  8  4
+
+Python Damian / simples / ejercicio17.py 5  5  6
+Python Damian / simples / ejercicio17.py 5  7  9
+python Damian / simples / ejercicio17.py 5  5  4
+
+python Damian / simples / ejercicio18.py 75  85  70
+python Damian / simples / ejercicio18.py 75  90  100
+python Damian / simples / ejercicio18.py 85  75  45
+
+python Damian / simples / ejercicio19.py 25  15  8
+python Damian / simples / ejercicio19.py 25  20  8
+python Damian / simples / ejercicio19.py 28  17  15
+
+python Damian / simples / ejercicio20.py 35  85  15
+python Damian / simples / ejercicio20.py 35  100  25
+python Damian / simples / ejercicio20.py 35  80  45
+
+python Damian / múltiplos / ejercicio21.py " Luis "  400  250  150
+python Damian / múltiplos / ejercicio21.py " Luis "  405  100  120
+python Damian / multiples / ejercicio21.py " luis "  405  120  150
+
+python Damian / multiples / ejercicio22.py 15  45  25
+python Damián / multiples / ejercicio22.py 15  40  15
+python Damián / multiples / ejercicio22.py 15  30  45
+
+python Damián / multiples / ejercicio23.py 25  15  35
+python Damián / multiples / ejercicio23.py 25  40  25
+python Damián / multiples / ejercicio23.py 25  40  40
+
+python Damian / múltiplos / ejercicio24.py 120  150  48
+python Damian / múltiplos / ejercicio24.py 120  175  85
+python Damian / múltiplos / ejercicio24.py 120  150  145
+
+python Damián / multiples / ejercicio25.py 25  45  75
+python Damián / multiples / ejercicio25.py 25  75  45
+python Damián / multiples / ejercicio25.py 25  80  45
+
+python Damián / multiples / ejercicio26.py 60  25  48
+python Damián / multiples / ejercicio26.py 45  25  85
+python Damián / multiples / ejercicio26.py 25  45  15
+
+python Damián / multiples / ejercicio27.py 20  20  20
+python Damián / multiples / ejercicio27.py 15  14  20
+python Damián / multiples / ejercicio27.py 15  20  20
+
+python Damián / multiples / ejercicio28.py 15  5  8
+python Damian / multiples / ejercicio28.py 15  10  8
+python Damian / multiples / ejercicio28.py 10  5  5
+
+python Damian / multiples / ejercicio29.py " antony "  45  55  20
+python Damian / multiples / ejercicio29.py " antony "  45  45  20
+python Damian / multiples / ejercicio29.py " antony "  45  50  14
+
+python Damian / multiples / ejercicio30.py " Karen "  8  10  12
+python Damian / multiples / ejercicio30.py " Karen "  8  8  5
+python Damian / multiples / ejercicio30.py " Karen "  8  8  10
+
+
+
