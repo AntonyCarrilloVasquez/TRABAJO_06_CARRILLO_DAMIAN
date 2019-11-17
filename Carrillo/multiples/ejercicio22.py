0b9d0b4e19d8a5e81d58f4df7bbf2fcfcdebad1b
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:33:34 -0500

    se agregó el ejercicio22

---
+++ Carrillo/multiples/ejercicio22.py
@@ -0,0 +1,26 @@
+import os
+#imput
+velocidad1=int(os.sys.argv[1])
+velocidad2=int(os.sys.argv[2])
+distancia=int(os.sys.argv[3])
+#processing
+tiempo_encuentro=distancia/(velocidad1*velocidad2)
+#verificador
+verificar=(tiempo_encuentro<=127)
+#output
+print("#####################")
+print(" Tiempo de encuentro de dos autos ")
+print("#####################")
+print("#")
+print("velocidad1:            ",velocidad1," ")
+print("velocidad2:            ",velocidad2," ")
+print("distancia:             ",distancia," ")
+print("tiempo de encuentro    ",tiempo_encuentro," ")
+print("#####################")
+#condicionales multiples:
+if (verificar==True):
+    print("casi se chocan :'v")
+if(tiempo_encuentro>21):
+    print("tengan cuidado")
+if(tiempo_encuentro<34):
+    print("acelera")
