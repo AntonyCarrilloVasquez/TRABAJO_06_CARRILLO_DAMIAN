d40b1205de31ccb5506da9818083812bea22c73a
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-16 21:27:19 -0500

    se agregó el ejercicio14

---
+++ Carrillo/dobles/ejercicio14.py
@@ -0,0 +1,24 @@
+import os
+#imput
+cliente=os.sys.argv[1]
+producto=os.sys.argv[2]
+cantidad=int(os.sys.argv[3])
+precio=int(os.sys.argv[4])
+#processing
+total=(cantidad*precio)
+#verificador
+verificar=(total>=176)
+#output
+print("#################")
+print(" boleta de compra ")
+print("#################")
+print("#")
+print("cliente:        ",cliente,"    ")
+print("producto:       ",producto,"   ")
+print("cantidad:       ",cantidad,"   ")
+print("precio:         ",precio  ,"   ")
+#condicional simple
+if(total>75):
+    print("te ganaste un desodorante")
+if(total<75):
+    print("gracias por su preferencia")
